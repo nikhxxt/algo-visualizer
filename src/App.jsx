036ae3c2ee4778e6c8ba87897f2b05e69dd4b5b9
nikhxxt <nikhxtwork@gmail.com>
2025-06-04 15:48:23 +0530

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import SortingVisualizer from "./components/SortingVisualizer";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <SortingVisualizer />
+    </div>
+  );
+};
+
+export default App;
