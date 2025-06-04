65ec14c8766b551cc4a96d7170440717346b6cf5
nikhxxt <nikhxtwork@gmail.com>
2025-06-04 15:50:34 +0530

    Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav style={{
+      padding: "1rem",
+      backgroundColor: "#282c34",
+      color: "white",
+      textAlign: "center",
+      fontSize: "1.5rem",
+      fontWeight: "bold"
+    }}>
+      AlgoViz - Algorithm Visualizer
+    </nav>
+  );
+};
+
+export default Navbar;
