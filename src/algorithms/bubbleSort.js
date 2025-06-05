da16382cac238b2502dcecd7720b95028974a649
nikhxxt <nikhxtwork@gmail.com>
2025-06-05 18:00:30 +0530

    bubbleSort.js

--- src/algorithms/bubbleSort.js
+++ src/algorithms/bubbleSort.js
@@ -1,9 +1,13 @@
+// Named export of bubbleSort function
 export function bubbleSort(arr) {
   let n = arr.length;
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; // swap
+        // swap arr[j] and arr[j+1]
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }
